a8bc1bc088a11bcb9309f827ce4791c24eadae7a
Jean Bovet <jean_bovet@mac.com>
2022-03-13 22:32:54 -0700

    Updated Layout B

--- BTrain/Layouts/Predefined/LayoutBCreator.swift
+++ BTrain/Layouts/Predefined/LayoutBCreator.swift
@@ -39,11 +39,11 @@ final class LayoutBCreator: LayoutCreating {
 
         // Blocks
 
-        let b_1 = Block("b1", type: .station, center: CGPoint(x: 340.0, y: 140.0), rotationAngle: -.pi)
-        let b_2 = Block("b2", type: .free, center: CGPoint(x: 110.0, y: 40.0), rotationAngle: .pi)
-        let b_3 = Block("b3", type: .free, center: CGPoint(x: 110.0, y: 140.0), rotationAngle: 0.0)
-        let b_4 = Block("b4", type: .free, center: CGPoint(x: 340.0, y: 40.0), rotationAngle: 0.0)
-        l.add([b_1,b_2,b_3,b_4])
+        let b_b1 = Block("b1", type: .station, center: CGPoint(x: 380.0, y: 150.0), rotationAngle: -.pi, waitingTime: 10.0)
+        let b_b2 = Block("b2", type: .free, center: CGPoint(x: 150.0, y: 50.0), rotationAngle: .pi, waitingTime: 10.0)
+        let b_b3 = Block("b3", type: .free, center: CGPoint(x: 150.0, y: 150.0), rotationAngle: 0.0, waitingTime: 10.0)
+        let b_b4 = Block("b4", type: .free, center: CGPoint(x: 380.0, y: 50.0), rotationAngle: 0.0, waitingTime: 10.0)
+        l.add([b_b1,b_b2,b_b3,b_b4])
 
         // Feedbacks
 
@@ -56,28 +56,28 @@ final class LayoutBCreator: LayoutCreating {
         let f_f41 = Feedback("f41", deviceID: 4, contactID: 1)
         let f_f42 = Feedback("f42", deviceID: 4, contactID: 2)
         l.feedbacks.append(contentsOf: [f_f11,f_f12,f_f21,f_f22,f_f31,f_f32,f_f41,f_f42])
-        l.assign(b_1, [f_f11,f_f12])
-        l.assign(b_2, [f_f21,f_f22])
-        l.assign(b_3, [f_f31,f_f32])
-        l.assign(b_4, [f_f41,f_f42])
+        l.assign(b_b1, [f_f11,f_f12])
+        l.assign(b_b2, [f_f21,f_f22])
+        l.assign(b_b3, [f_f31,f_f32])
+        l.assign(b_b4, [f_f41,f_f42])
 
         // Turnouts
 
-        let t_t12 = Turnout("t1", type: .doubleSlip2, address: .init(0, .MM), address2: .init(0, .MM), state: .straight, center: CGPoint(x: 230.0, y: 90.0), rotationAngle: -2.356194490192345)
-        l.turnouts.append(contentsOf: [t_t12])
+        let t_t1 = Turnout("t1", type: .doubleSlip2, address: .init(0,.MM), address2: .init(0,.MM), state: .straight, center: CGPoint(x: 270.0, y: 100.0), rotationAngle: -2.356194490192345)
+        l.turnouts.append(contentsOf: [t_t1])
 
         // Transitions
 
-        l.link(from: b_1.next, to: t_t12.socket0)
-        l.link(from: t_t12.socket1, to: b_2.previous)
-        l.link(from: b_2.next, to: b_3.previous)
-        l.link(from: b_3.next, to: t_t12.socket2)
-        l.link(from: t_t12.socket3, to: b_4.previous)
-        l.link(from: b_4.next, to: b_1.previous)
+        l.link(from: b_b1.next, to: t_t1.socket0)
+        l.link(from: t_t1.socket1, to: b_b2.previous)
+        l.link(from: b_b2.next, to: b_b3.previous)
+        l.link(from: b_b3.next, to: t_t1.socket2)
+        l.link(from: t_t1.socket3, to: b_b4.previous)
+        l.link(from: b_b4.next, to: b_b1.previous)
 
         // Routes
 
-        l.newRoute("r1", name: "Loop", [Route.Step(b_1,.next),Route.Step(b_2,.next),Route.Step(b_3,.next),Route.Step(b_4,.next),Route.Step(b_1,.next)])
+        l.newRoute("r1", name: "Loop", [Route.Step(b_b1,.next, nil),Route.Step(b_b2,.next, nil),Route.Step(b_b3,.next, nil),Route.Step(b_b4,.next, nil),Route.Step(b_b1,.next, nil)])
 
         // Trains
         l.addTrain(.init(uuid: "1", name: "Rail 2000"))
