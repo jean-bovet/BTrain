c3415ead274d533fcb44d2040fca6f2581849634
Jean Bovet <jean_bovet@mac.com>
2022-04-11 21:51:30 -0700

    Improved toolbar readability

--- BTrain/Views/Document/DocumentToolbarContent.swift
+++ BTrain/Views/Document/DocumentToolbarContent.swift
@@ -62,36 +62,34 @@ struct ConnectCommandsView: View {
     
     var body: some View {
         if document.connected {
+            Button("Disconnect") {
+                document.disconnect()
+            }
+            .foregroundColor(.red)
+
+            Spacer()
+            
             SimulatorIndicationView(simulator: document.simulator)
                         
             Spacer()
 
-            Button("􀋧") {
+            Button("Go") {
                 document.enable() {}
             }
             .disabled(!document.connected)
             .help("Enable Power")
             .foregroundColor(.green)
 
-            Button("􀋫") {
+            Button("Stop") {
                 document.disable() {}
             }
             .disabled(!document.connected)
             .help("Disable Power")
             .foregroundColor(.red)
-            
-            Spacer()
-
-            Button("􀡷") {
-                document.disconnect()
-            }
-            .help("Disconnect")
-            .foregroundColor(.red)
         } else {
-            Button("􀡷") {
+            Button("Connect") {
                 self.connectAlertShowing.toggle()
             }
-            .help("Connect")
             .foregroundColor(.green)
         }
     }
